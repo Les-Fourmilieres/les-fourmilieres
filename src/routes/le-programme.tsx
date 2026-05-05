8697fe20cdaf7dc4f8b1caa0852b4a729a9c7c1e
Arthur Lenoir <me@arthurlenoir.fr>
2026-05-05 15:49:56 +0200

    style: improve iframe integration

--- src/routes/le-programme.tsx
+++ src/routes/le-programme.tsx
@@ -20,11 +20,13 @@ function RouteComponent() {
         height="550"
         src="https://831d7d2b.sibforms.com/serve/MUIFAGRLxcnC6V1RZbJo8HRFs16TSK5o7YcHGuBQFXk9UDZOHZxjnGTi6DcChRJlbQ-TM_todKH3Sm7SGYWsRm3bnHYgrDldw3YrTy1OmoLqJVGcX8uXMXQbyv1l71ADn25xNdJHH7iHDApLfM8TkSoA1wyOLI0wpEQcdt9S_RKKeEAHep6xsJhsyFzE4MQEWzwKPFmhQ4id_3mx"
         allowFullScreen
+        allowTransparency={true}
         style={{
           display: "block",
           margin: "0 auto",
           maxWidth: "100%",
           border: "none",
+          backgroundColor: "transparent",
         }}
       ></iframe>
     </Section>
