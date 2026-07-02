9e1dde046e0a4981ff72d120abedd4d6bb3b66a5
Arthur Lenoir <me@arthurlenoir.fr>
2026-07-02 15:34:29 +0200

    fix: pages meta data

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,42 @@
 import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
 import { Header } from "../components/Header/Header";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  head: () => ({
+    meta: [
+      { title: `🐜 Les Fourmilières · Festival antifasciste et solidaire` },
+      {
+        name: "description",
+        content: `Les Fourmilières est un festival solidaire et antifasciste. Nous sommes des collectifs, associations, librairies, brasseries, syndicats, lieux culturels, troupes de théâtre, associations sportives, tiers lieux, cinémas. Nous sommes actuellement une centaine de collectifs répartis du Larzac aux Cévennes et jusqu’au littoral, sur l’Aveyron, le Gard et l’Hérault.`,
+      },
+      {
+        property: "og:title",
+        content: `🐜 Les Fourmilières · Festival antifasciste et solidaire`,
+      },
+      {
+        property: "og:description",
+        content: `Les Fourmilières est un festival solidaire et antifasciste. Nous sommes des collectifs, associations, librairies, brasseries, syndicats, lieux culturels, troupes de théâtre, associations sportives, tiers lieux, cinémas. Nous sommes actuellement une centaine de collectifs répartis du Larzac aux Cévennes et jusqu’au littoral, sur l’Aveyron, le Gard et l’Hérault.`,
+      },
+      { property: "og:type", content: "website" },
+      {
+        property: "og:image",
+        content: "https://les-fourmilieres.org/les-fourmilieres-preview.webp",
+      },
+      {
+        property: "og:locale",
+        content: "fr_FR",
+      },
+    ],
+  }),
 });
 
 // eslint-disable-next-line react-refresh/only-export-components
 function RootComponent() {
   return (
     <React.Fragment>
+      <HeadContent />
       <Header />
       <main role="main">
         <Outlet />
