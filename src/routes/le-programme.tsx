b594ec255ca8f1117e706e94d8138c2171a4240a
tha <tha@kelis.fr>
2026-07-10 10:39:04 +0200

    add warning para on calendar page

--- src/routes/le-programme.tsx
+++ src/routes/le-programme.tsx
@@ -11,6 +11,11 @@ function RouteComponent() {
   return (
     <Section>
       <h1>Le Programme</h1>
+			<p>
+				Le programme est en cours de construction. Il sera complété ici au fur et à
+				mesure que les événements serons mis en ligne par les collectifs organisateurs.
+				Au total, nous prévoyons plus de 200 événements organisés par plus de 100 collectifs
+				répartis dans les départements de l'Aude, l'Hérault, le Gard, la Lozère et l'Aveyron.</p>
       <Agenda />
     </Section>
   );
