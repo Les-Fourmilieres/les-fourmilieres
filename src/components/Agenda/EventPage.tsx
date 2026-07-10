f94fb809c975ab22c8f45393cb54d9a976d1d885
Arthur Lenoir <me@arthurlenoir.fr>
2026-07-10 11:35:57 +0200

    style: improve event page for mobile

--- src/components/Agenda/EventPage.tsx
+++ src/components/Agenda/EventPage.tsx
@@ -5,12 +5,31 @@ import { EventDate } from "./EventDate";
 import { EventTime } from "./EventTime";
 import { ParticipateButton } from "./ParticipateButton";
 import { EventAddress } from "./EventAddress";
+import { useMediaQuery } from "react-responsive";
+
+const StyledSection = styled(Section)`
+  padding-top: 24px;
+`;
 
 const EventHeader = styled.div`
   align-self: stretch;
   display: flex;
   gap: 16px;
   justify-content: flex-start;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+`;
+
+const DateAndTime = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  align-self: stretch;
 `;
 
 const Title = styled.h1`
@@ -34,6 +53,18 @@ const EventCover = styled.aside`
   box-sizing: border-box;
   align-items: flex-end;
   justify-content: flex-end;
+
+  @media (max-width: 800px) {
+    height: 250px;
+  }
+`;
+
+const TimeAndAddress = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  flex: 1 1 auto;
 `;
 
 interface Props {
@@ -41,6 +72,7 @@ interface Props {
 }
 
 export function EventPage({ event }: Props) {
+  const isMobile = useMediaQuery({ maxWidth: 800 });
   return (
     <>
       <EventCover
@@ -48,20 +80,34 @@ export function EventPage({ event }: Props) {
           backgroundImage: `url(${event.picture?.url ?? "https://agenda.les-fourmilieres.org/media/5d51acc4f1d82879973317de10ae2811f51e947d17923b84d95ea2b69a939adf.webp"})`,
         }}
       ></EventCover>
-      <Section>
-        <EventHeader>
-          <EventDate event={event} />
-          <div>
+      <StyledSection>
+        {isMobile ? (
+          <EventHeader>
             <Title>{event.title}</Title>
-            <EventTime event={event} />
-            <EventAddress event={event} />
-          </div>
-          <ParticipateButton event={event} />
-        </EventHeader>
+            <DateAndTime>
+              <EventDate event={event} />
+              <TimeAndAddress>
+                <EventTime event={event} />
+                <EventAddress event={event} />
+              </TimeAndAddress>
+            </DateAndTime>
+            <ParticipateButton event={event} />
+          </EventHeader>
+        ) : (
+          <EventHeader>
+            <EventDate event={event} />
+            <div>
+              <Title>{event.title}</Title>
+              <EventTime event={event} />
+              <EventAddress event={event} />
+            </div>
+            <ParticipateButton event={event} />
+          </EventHeader>
+        )}
         {event.description && (
           <Content dangerouslySetInnerHTML={{ __html: event.description }} />
         )}
-      </Section>
+      </StyledSection>
     </>
   );
 }
