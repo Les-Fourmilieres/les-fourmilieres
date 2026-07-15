0a37b303eda6b5b9eed13fb4f218175d3c56df64
Arthur Lenoir <me@arthurlenoir.fr>
2026-07-16 00:11:41 +0200

    fix: download CP link

--- src/routes/presse.tsx
+++ src/routes/presse.tsx
@@ -1,40 +1,35 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Section } from "../components/Section";
-import {Link} from "../components/Link/Link.js";
+import { Link } from "../components/Link/Link.js";
+import cp26 from "../assets/presse/CP_Les_Fourmilieres26.pdf?url";
 
 export const Route = createFileRoute("/presse")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 // eslint-disable-next-line react-refresh/only-export-components
 function RouteComponent() {
-	return (
-		<Section>
-			<h1>
-				Espace presse
-			</h1>
-			<Section>
-				<h2>
-					Communiqués de presse
-				</h2>
-				<p>
-						Télécharger le{" "}
-						<Link href="presse/CP_Les_Fourmilieres26.pdf">
-							communiqué de presse du 15 juillet
-						</Link>
-					.
-				</p>
-			</Section>
-			<Section>
-				<h2>
-					Contact presse
-				</h2>
-				<p>
-					<Link href="mailto:presse@les-fourmilieres.org">
-						presse@les-fourmilieres.org
-					</Link>
-				</p>
-			</Section>
-		</Section>
-	);
+  return (
+    <Section>
+      <h1>Espace presse</h1>
+      <Section>
+        <h2>Communiqués de presse</h2>
+        <p>
+          Télécharger le{" "}
+          <Link href={cp26} download="CP_Les_Fourmilieres_26.pdf">
+            communiqué de presse du 15 juillet
+          </Link>
+          .
+        </p>
+      </Section>
+      <Section>
+        <h2>Contact presse</h2>
+        <p>
+          <Link href="mailto:presse@les-fourmilieres.org">
+            presse@les-fourmilieres.org
+          </Link>
+        </p>
+      </Section>
+    </Section>
+  );
 }
