a056711596740d9ecd8fe43608feb8183e0775fe
tha <tha@kelis.fr>
2026-08-28 16:17:07 +0200

    clean up communique presse

--- src/routes/presse.tsx
+++ src/routes/presse.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Section } from "../components/Section";
-import cp26 from "../assets/presse/CP_Les_Fourmilieres26.pdf?url";
+import { Link } from "../components/Link/Link.js";
 import dp from "../assets/presse/DP_Fourmilières.pdf?url";
 import styled from "styled-components";
 
@@ -25,17 +25,17 @@ function RouteComponent() {
 					<p>
 						Télécharger le{" "}
 						<CpLink href={dp} target="_blank">
-							dossier de presse
+							dossier d epresse
 						</CpLink>
 						.
 					</p>
-        <h2>Communiqués de presse</h2>
+      </Section>
+      <Section>
+        <h2>Contact presse</h2>
         <p>
-          Télécharger le{" "}
-          <CpLink href={cp26} target="_blank">
-            communiqué de presse du 15 juillet
-          </CpLink>
-          .
+          <Link href="mailto:presse@les-fourmilieres.org">
+            presse@les-fourmilieres.org
+          </Link>
         </p>
       </Section>
     </Section>
