a8220eeebe5aaa0529fa0a439d31338a6219dfe6
tha <tha@kelis.fr>
2026-09-08 11:56:01 +0200

    review teaser

--- src/views/Teaser.tsx
+++ src/views/Teaser.tsx
@@ -13,7 +13,7 @@ export function Teaser() {
   return (
     <Container>
       <Section>
-        <h1>C'est quoi Les Fourmilières ?</h1>
+        <h2>C'est quoi Les Fourmilières ?</h2>
 
         <p>
           Les Fourmilières est un{" "}
@@ -49,8 +49,43 @@ export function Teaser() {
           <strong>décentralisé et fonctionne de façon horizontale</strong>, sans
           association centralisant son organisation.
         </p>
-        <Map collectifs={collectifs} />
       </Section>
+			<Section>
+				<h2>Pourquoi ce festival ?</h2>
+				<p>
+					Le festival Les Fourmilières est né de l’envie, du besoin et, désormais,{" "}
+					de l'urgence aussi, de se retrouver, de créer du lien avec d’autres, de
+					rêver - en toute lucidité - un monde plus solidaire.
+				</p>
+				<p>
+					<strong>
+						L’objectif du festival est de clamer la lutte contre l’extrême-droite,{" "}
+						notamment en démontrant que de nombreux acteurs proposent, de manière
+						argumentée, un autre projet de société face à la menace.
+					</strong>
+				</p>
+				<p>
+					Il est antifasciste et solidaire parce qu’il refuse les idées violentes
+					et réactionnaires de l’extrême-droite. La menace, réelle autant que
+					croissante, fait que nous sommes des millions à constater que le monde
+					actuel et, pire encore, l’avenir qui se profile, préfigure une société
+					individualiste, discriminante, haineuse, liberticide : la dédiabolisation
+					des discours les plus radicaux, les lois iniques, le recul de la démocratie,{" "}
+					le monopole des médias aux mains de quelques milliardaires qui promeuvent
+					les idées réactionnaires, la manipulation de la pensée et l’inversion des
+					concepts, la violence d’état (symbolique et réelle), les génocides, les
+					écocides, les injustices…
+				</p>
+				<p>
+					Aujourd’hui, une centaine de collectifs se retrouvent autour d’un objectif
+					commun : imaginer, de manière constructive, réfléchie et positive, un monde
+					plus juste, plus beau, plus empathique, en tout état de cause, plus
+					indispensable que jamais.
+				</p>
+			</Section>
+			<Section>
+				<Map collectifs={collectifs} />
+			</Section>
     </Container>
   );
 }
