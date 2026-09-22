ac97d743db721c6229210ef4e6b442c0b74fb468
Arthur Lenoir <me@arthurlenoir.fr>
2026-09-22 10:18:49 +0200

    feat: add presse content

--- src/routes/presse.tsx
+++ src/routes/presse.tsx
@@ -4,6 +4,17 @@ import { Link } from "../components/Link/Link.tsx";
 import dp from "../assets/presse/DP_Fourmilières.pdf?url";
 import styled from "styled-components";
 
+const Row = styled.div`
+  display: flex;
+  gap: 18px;
+  flex-wrap: wrap;
+  width: 100%;
+  ${Section} {
+    flex: 1 1 45%;
+    min-width: 300px;
+  }
+`;
+
 const CpLink = styled.a`
   &,
   &:visited {
@@ -20,24 +31,45 @@ function RouteComponent() {
   return (
     <Section>
       <h1>Espace presse</h1>
-      <Section>
-				<h2>Dossier de presse</h2>
-					<p>
-						Télécharger le{" "}
-						<CpLink href={dp} target="_blank">
-							dossier de presse
-						</CpLink>
-						.
-					</p>
-      </Section>
-      <Section>
-        <h2>Contact presse</h2>
-        <p>
-          <Link href="mailto:presse@les-fourmilieres.org">
-            presse@les-fourmilieres.org
-          </Link>
-        </p>
-      </Section>
+      <Row>
+        <Section>
+          <h2>Dossier de presse</h2>
+          <p>
+            Télécharger le{" "}
+            <CpLink href={dp} target="_blank">
+              dossier de presse
+            </CpLink>
+            .
+          </p>
+        </Section>
+        <Section>
+          <h2>Contact presse</h2>
+          <p>
+            <Link href="mailto:presse@les-fourmilieres.org">
+              presse@les-fourmilieres.org
+            </Link>
+          </p>
+        </Section>
+      </Row>
+
+      <h2>Extraits de presse</h2>
+      <h3>18 septembre 2026 · La Marseillaise</h3>
+      <CpLink href="/presse/260918_LaMarseillaise.pdf" target="_blank">
+        Un festival fait fourmiller les initiatives contre l'extrême droite
+      </CpLink>
+      <h3>04 septembre 2026 · Midi Libre Lodève</h3>
+      <CpLink href="/presse/260904_Midi_Libre_Lodeve.jpg" target="_blank">
+        Les Fourmilières ont travaillé tout l'été pour préparer le festival
+      </CpLink>
+      <h3>04 juillet 2026 · Midi Libre Lodève</h3>
+      <CpLink href="/presse/260704_MidiLibreLodeve.pdf" target="_blank">
+        Le Parti Communiste interpelle le maire sur sa neutralité
+      </CpLink>
+      <h3>04 juillet 2026 · Midi Libre Lodève</h3>
+      <CpLink href="/presse/260704_MidiLibreLodeve_3.pdf" target="_blank">
+        Le festival antifasciste et antiraciste Les Foumilières n'est pas le
+        bienvenu à Lodève
+      </CpLink>
     </Section>
   );
 }
