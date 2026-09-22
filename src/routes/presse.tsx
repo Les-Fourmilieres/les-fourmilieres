f79b602d2da2c49d28a16b482cd3058947f852b1
Arthur Lenoir <me@arthurlenoir.fr>
2026-09-22 10:38:12 +0200

    feat: add articles to presse

--- src/routes/presse.tsx
+++ src/routes/presse.tsx
@@ -9,6 +9,7 @@ const Row = styled.div`
   gap: 18px;
   flex-wrap: wrap;
   width: 100%;
+  align-items: flex-start;
   ${Section} {
     flex: 1 1 45%;
     min-width: 300px;
@@ -41,6 +42,14 @@ function RouteComponent() {
             </CpLink>
             .
           </p>
+          <p style={{ textAlign: "center" }}>
+            Pour obtenir le mot de passe vous permettant de télécharger les
+            photos de presse, merci d'envoyer un email à{" "}
+            <Link href="mailto:presse@les-fourmilieres.org">
+              presse@les-fourmilieres.org
+            </Link>
+            .
+          </p>
         </Section>
         <Section>
           <h2>Contact presse</h2>
@@ -51,12 +60,24 @@ function RouteComponent() {
           </p>
         </Section>
       </Row>
-
       <h2>Extraits de presse</h2>
       <h3>18 septembre 2026 · La Marseillaise</h3>
       <CpLink href="/presse/260918_LaMarseillaise.pdf" target="_blank">
         Un festival fait fourmiller les initiatives contre l'extrême droite
       </CpLink>
+      <h3>16 septembre 2026 · Renverser la table</h3>
+      <CpLink href="/presse/260916_RenverserLaTable.pdf" target="_blank">
+        Newsletter · Renverser la table par Victoire Tuaillon
+      </CpLink>
+      <h3>11 septembre 2026 · Libération</h3>
+      <CpLink href="/presse/260911_Liberation.pdf" target="_blank">
+        Festival « Les Fourmilières » creuse le sillon explicite d'un festival antifasciste et solidaire
+      </CpLink>
+      <h3>11 septembre 2026 · Médiapart</h3>
+      <CpLink href="/presse/260911_Mediapart_InExtremis.pdf" target="_blank">
+        In Extremis : la newsletter sur les extrêmes droites
+      </CpLink>
+      
       <h3>04 septembre 2026 · Midi Libre Lodève</h3>
       <CpLink href="/presse/260904_Midi_Libre_Lodeve.jpg" target="_blank">
         Les Fourmilières ont travaillé tout l'été pour préparer le festival
